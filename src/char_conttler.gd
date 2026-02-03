99810dcb28d491cab8fa9319c2ea987a0499b3df
TheAmericanEmu <harringtondingc@gmail.com>
2026-02-03 12:40:58 -0600

    finally added the freaking movement

--- src/char_conttler.gd
+++ src/char_conttler.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 @export var mous_sens :=0.1
 @export var speed :=10
 
+
 var move_dir: Vector2 # Input direction for movement
 var look_dir: Vector2 # Input direction for look/aim
 
@@ -19,21 +20,17 @@ func _ready():
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		self.rotate_y(-event.relative.x*0.01)
+		cam.rotate_y(-event.relative.x*0.01)
 		var pitch_rotate = cam.rotation_degrees.x - event.relative.y* mous_sens
-		var new_pitch = clampf(pitch_rotate,-100,100)
+		var new_pitch = clampf(pitch_rotate,-90,90)
 
 		cam.rotation_degrees.x=new_pitch
 
 func _physics_process(delta: float):
-	move_dir = Input.get_vector("right","left","back","foward")
-
-	var forward = Vector3(move_dir.x,0,move_dir.y)*cam.global_transform.basis
-	var walk_dir = Vector3(forward.x, 0, forward.z).normalized()
-	walk_vel = walk_vel.move_toward(walk_dir * speed * move_dir.length(),100*delta)
-	if not is_on_floor():
-		velocity.y=-1
-	velocity=walk_vel
+	move_dir = Input.get_vector("left","right","foward","back")
+	print(basis)
+	var forward = Vector3(move_dir.x,0,move_dir.y)*cam.transform.basis*speed
+	velocity=forward
 	move_and_slide()
 	
 	
---
+++ src/character_body_3d_mk2.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+@export var cam:Node3D
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export var mous_sen:=0.1
+
+func _ready():
+# Capture the mouse for FPS-style control
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		cam.rotate_y(-event.relative.x*0.01)
+		var pitch_rotate = cam.rotation_degrees.x - event.relative.y* mous_sen
+		var new_pitch = clampf(pitch_rotate,-90,90)
+
+		cam.rotation_degrees.x=new_pitch
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "foward", "back")
+	var direction := (cam.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
